f4183198f8ffb41d0ef52ea4ea24a9670f5c8e0b
maxdouglass <77952746+maxdouglass@users.noreply.github.com>
2024-04-23 10:02:41 -0500

    Add files via upload

---
+++ microsite/final/slick-setup.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('.character-cards').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+        ]
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const navLinks = document.querySelectorAll('nav ul li a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            navLinks.forEach(l => l.classList.remove('active'));
+            this.classList.add('active');
+        });
+    });
+});
+
